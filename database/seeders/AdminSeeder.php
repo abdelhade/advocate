0af4786a33f72a6210550a74ee48f1b649b70b4d
abdelhade <abdelhadeeladawy@gmail.com>
2026-09-14 17:46:10 +0300

    feat: add AdminSeeder to populate default admin accounts

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -4,16 +4,27 @@ namespace Database\Seeders;
 
 use App\Models\Admin;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
     public function run(): void
     {
+        // Admin 1: abdelhade
+        Admin::updateOrCreate(
+            ['username' => 'abdelhade'],
+            [
+                'name' => 'Abdelhade',
+                'password' => Hash::make('Aa1234###'),
+            ]
+        );
+
+        // Admin 2: hadi
         Admin::updateOrCreate(
             ['username' => 'hadi'],
             [
-                'name' => 'hadi',
-                'password' => '00000000',
+                'name' => 'Hadi',
+                'password' => Hash::make('Aa1234###'),
             ]
         );
     }
